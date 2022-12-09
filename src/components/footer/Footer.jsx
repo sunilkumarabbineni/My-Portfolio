42f272108575261f02da4973afedc60239e4f076
Abbineni Sunil Kumar <abbinenisunilkumar003@gmail.com>
2022-12-10 00:39:00 +0530

    footer error resolved

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,9 +10,9 @@ import {FaLinkedinIn} from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">Sunil Kumar Abbineni</a>
+      <a href="#footer" className="footer__logo">Sunil Kumar Abbineni</a>
         <ul className="permalink">
-          <li><a href="#">Home</a></li>
+          <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#experience">Experience</a></li>
           <li><a href="#services">Services</a></li>
